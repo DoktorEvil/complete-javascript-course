0e9dc83d15983efdc7a8c4cc80d06f325b3e424b
DoktorEvil <37379653+DoktorEvil@users.noreply.github.com>
2025-03-31 19:02:29 -0400

    Updates

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -4,3 +4,11 @@ if (js === "amazing") alert("JavaScipt is FUN!");
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Todder");
+console.log(23)
+
+let firstName = "TodderVar";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
